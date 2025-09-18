0481e45de2429459531dd9f7dc33328ec83a0477
Diego Mejía <dmejiaguevara@gmail.com>
2025-09-18 13:16:48 -0600

    Add Productos class and migrate index.html to index.jsp
    Creé la clase Productos. Reemplacé index.html con un JSP index.jsp. Creación del paquete srv para luego.

---
+++ PR02TAR01/src/main/java/productos/Productos.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package productos;
+
+/**
+ *
+ * @author dmeji
+ */
+public class Productos {
+    private int id;
+    private String nombre, descripcion;
+    private double precio;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getId() {
+        return id;
+    }
+    
+    
+}
